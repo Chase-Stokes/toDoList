1034e58aa75855d2fbc097e83f5140649cf4de8f
Chase Stokes <chasehunterstokes@gmail.com>
2022-02-28 13:06:24 -0800

    followed along with monday HW to implement a new UI

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -7,14 +7,14 @@ namespace ToDoList.Models
     public string Description {get; set;}
     private static List<Item> _instances = new List<Item> {};
 
-    public static List<Item> GetAll()
-    {
-      return _instances;
-    }
     public Item(string description)
     {
       Description = description;
       _instances.Add(this);
+    }
+    public static List<Item> GetAll()
+    {
+      return _instances;
     }
 
     public static void ClearAll()
